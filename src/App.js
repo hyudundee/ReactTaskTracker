c6ad3d2d518953660edeb438caad81408fd3c017
hyudundee <hyudundee@gmail.com>
2021-03-06 14:01:11 -0800

    tests useState move to Appjs level

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
-function App() {
+function App() {  
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctor Appointment',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Meeting at School',
+      day: 'Feb 6th at 1:30pm',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'Food shopping',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    }
+  ])
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
