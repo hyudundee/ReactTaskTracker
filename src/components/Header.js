57919d964cd220ac8a778a38683903cc6a7f02c1
hyudundee <hyudundee@gmail.com>
2021-03-06 13:32:16 -0800

    button and styling addedd

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('Click')
+  }
   return (
-    <header>
-      <h1 style={headingStyle}>{title}</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Hello' onClick={onClick}/>
     </header>
   )
 }
@@ -16,9 +21,11 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
+/*
+// css in JS
 const headingStyle = {
   color: 'red',
   backgroundColor: 'black'
 }
-
+*/
 export default Header
