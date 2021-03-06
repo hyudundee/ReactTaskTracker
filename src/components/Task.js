c1d696703e677aa04ae74bc6b0ff52a1f52ff4bb
hyudundee <hyudundee@gmail.com>
2021-03-06 14:10:14 -0800

    task component and delete icons from react-icons added

---
+++ src/components/Task.js
@@ -0,0 +1,15 @@
+// we are basically using icons as react component
+import { FaTimes } from 'react-icons/fa'
+
+const Task = ({ task }) => {
+  return (
+    <div className='task'>
+      <h3>
+        { task.text } <FaTimes style={{ color: 'red', cursor: 'pointer'}}/>
+      </h3>
+      <p>{ task.day }</p>
+    </div>
+  )
+}
+
+export default Task
