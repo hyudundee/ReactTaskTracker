3f0a8da3d90dc829b62982de7affc90950badcae
hyudundee <hyudundee@gmail.com>
2021-03-06 14:42:59 -0800

    Default message on no task added

--- src/App.js
+++ src/App.js
@@ -32,7 +32,9 @@ function App() {
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} onDelete={deleteTask}/>
+      {tasks.length > 0 ?
+         (<Tasks tasks={tasks} onDelete={deleteTask}/>)
+        :'No task to show'}
     </div>
   );
 }
